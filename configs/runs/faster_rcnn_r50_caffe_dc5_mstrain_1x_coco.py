e86892e2067e8985e8149685b5bd98a2b0332d46
ly-muc <linyan.yang@outlook.de>
2023-05-23 21:34:18 +0200

    add training configs

---
+++ configs/runs/faster_rcnn_r50_caffe_dc5_mstrain_1x_coco.py
@@ -0,0 +1,11 @@
+_base_ = [
+    '../_base_/models/faster_rcnn_r50_fpn.py',
+    '../_base_/datasets/dac_contest_base.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
+
+
+load_from = '/home/linyan/code/gpu_starter_2023/checkpoints/epoch_12.pth'
+
+# Set up working dir to save files and logs.
+work_dir = './tutorial_exps'
